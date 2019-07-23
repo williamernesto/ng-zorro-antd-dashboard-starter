c43d82592a68851096fe9579c237e34d06e6942d
williamernesto <william.ibar@gmail.com>
2019-07-23 17:17:30 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-   title = 'Title';
+export class AppComponent {
+  title = 'Title';
 
   visible = false;
 
